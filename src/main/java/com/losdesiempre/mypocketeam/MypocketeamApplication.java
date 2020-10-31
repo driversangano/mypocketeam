a96b65954dc13bb80a2057acfe2d2017eb204765
Pablo Contreras <pabloskylosky@gmail.com>
2020-10-31 14:22:53 -0300

    Initial commit

---
+++ src/main/java/com/losdesiempre/mypocketeam/MypocketeamApplication.java
@@ -0,0 +1,13 @@
+package com.losdesiempre.mypocketeam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypocketeamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypocketeamApplication.class, args);
+    }
+
+}
